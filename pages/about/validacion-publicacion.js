e957da2ce549bc461e6aed3bc48f38723533990b
AndresFr56 <andresfreirech56@outlook.com>
2024-11-21 18:13:13 -0500

    update form publi and validator
    update form publi and validator

--- pages/about/validacion-publicacion.js
+++ pages/about/validacion-publicacion.js
@@ -7,37 +7,37 @@ function validarPublicacion(){
     //Valida título de la publicación
     let titulo = document.getElementById("titulo_publicacion");
     if(titulo.value === ""){
-        cargarMensaje("El título no puede estar vacío", titulo);
+        cargarMensaje("*El título no puede estar vacío", titulo);
         esValido = false;
     }else if(titulo.value.length < 10){
-        cargarMensaje("El título debe tener mínimo 10 caracteres", titulo);
+        cargarMensaje("*El título debe tener mínimo 10 caracteres", titulo);
         esValido = false;
     }else if(!validarCaracteres.test(titulo.value)){
-        cargarMensaje("El título solo puede tener letras", titulo);
+        cargarMensaje("*El título solo puede tener letras", titulo);
         esValido = false;
     }
 
     //Valida tipo de publicación
     let tipoPublicacion = document.getElementById("tipo_publicacion");
     if(tipoPublicacion.value === ""){
-        cargarMensaje("Seleccione el tipo de publicacion", tipoPublicacion);
+        cargarMensaje("*Seleccione una publicación", tipoPublicacion);
         esValido = false;
     }
 
     //Valida descripción de la publicación
     let descripcion = document.querySelector(".publicacionDescripcion");
     if(descripcion.value === ""){
-        cargarMensaje("La descripción no puede estar vacía", descripcion);
+        cargarMensaje("*La descripción no puede estar vacía", descripcion);
         esValido = false;
     }else if(descripcion.value.length < 15){
-        cargarMensaje("La descripcion debe tener mínimo 15 caracteres", descripcion);
+        cargarMensaje("*La descripcion debe tener mínimo 15 caracteres", descripcion);
         esValido = false;
     }
 
     //Valida prioridad del evento
     let prioridad = document.querySelector('input[name="prioridad"]:checked');
     if(!prioridad){
-        cargarMensaje("Debe seleccionar una prioridad para su publicación", prioridad);
+        //cargarMensaje("*Debe seleccionar una prioridad para su publicación*", prioridad);
         esValido = false;
     }
 
@@ -45,12 +45,12 @@ function validarPublicacion(){
     let fechaPublicacion = document.getElementById("fecha_publicacion");
     let fechaActual = new Date();
     if(fechaPublicacion.value === ""){
-        cargarMensaje("La fecha es requerida", fechaPublicacion);
+        cargarMensaje("*La fecha es requerida", fechaPublicacion);
         esValido = false;
     }else{
         let fechaSeleccionada = new Date(fechaPublicacion.value);
         if(fechaSeleccionada > fechaActual){
-            cargarMensaje("La fecha selecionada no puede ser mayor a la fecha actual", fechaPublicacion);
+            cargarMensaje("*La fecha selecionada no puede ser mayor a la fecha actual", fechaPublicacion);
             esValido = false;
         }
     }
@@ -58,13 +58,13 @@ function validarPublicacion(){
     //Valida nombre del publicante
     let nombre = document.getElementById("nombre");
     if(nombre.value === ""){
-        cargarMensaje("El nombre no puede estar vacío", nombre);
+        cargarMensaje("*El nombre no puede estar vacío", nombre);
         esValido = false;
     }else if(nombre.value.length < 5){
-        cargarMensaje("El nombre debe tener minimo 5 caracteres", nombre);
+        cargarMensaje("*El nombre debe tener minimo 5 caracteres", nombre);
         esValido = false;
     }else if(!validarCaracteres.test(nombre.value)){
-        cargarMensaje("El nombre solo puede tener letras", nombre);
+        cargarMensaje("*El nombre solo puede tener letras", nombre);
         esValido = false;
     }
 
@@ -72,13 +72,13 @@ function validarPublicacion(){
     let telefono = document.getElementById("telefono");
     let telefonoRegex = /^[0-9]+$/;
     if(telefono.value === ""){
-        cargarMensaje("El teléfono no puede estar vacío", telefono);
+        cargarMensaje("*El teléfono no puede estar vacío", telefono);
         esValido = false;
     }else if(telefono.value.length !== 10){
-        cargarMensaje("El teléfono debe tener 10 digitos", telefono);
+        cargarMensaje("*El teléfono debe tener 10 digitos", telefono);
         esValido = false;
     }else if(!telefonoRegex.test(telefono.value)){
-        cargarMensaje("El teléfono solo puede tener números", telefono);
+        cargarMensaje("*El teléfono solo puede tener números", telefono);
         esValido = false;
     }
 
@@ -86,13 +86,13 @@ function validarPublicacion(){
     let correo = document.querySelector('.campoCorreo input');
     let correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if(correo.value === ""){
-        cargarMensaje("El correo electrónico es requerido", correo);
+        cargarMensaje("*El correo electrónico es requerido", correo);
         esValido = false;
     }else if(!correoRegex.test(correo.value)){
-        cargarMensaje("El correo electrónico no es válido", correo);
+        cargarMensaje("*El correo electrónico no es válido", correo);
         esValido = false;
     }
-    
+
     return esValido;
 }
 
