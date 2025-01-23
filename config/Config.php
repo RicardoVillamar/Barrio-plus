4729540eedbe9385c285c424a86fd2d15eacfd9c
AndresFr56 <andresfreirech56@outlook.com>
2025-01-22 22:52:01 -0500

    Constantes para la vista publicacion y bd
    Constantes para la vista publicacion y conexion de la bd

--- config/Config.php
+++ config/Config.php
@@ -2,3 +2,11 @@
 //controlador y funcion predefinida
 define("CONTROLADOR_PRINCIPAL", "index");
 define("FUNCION_PRINCIPAL", "index");
+
+//ruta de vistas modulo de publicaciones
+define("VCATEGORIAS", "view/publicaciones/publicaciones.");
+
+//conexion con la bd
+define("DBNAME","mvc");
+define("DBUSER","root");
+define("DBPASSWORD","");
