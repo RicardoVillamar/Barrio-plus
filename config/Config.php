f957a0a268f17b03fbfb00f7678a3eb3f52f194d
Yovel-Kyr <ajoel232003@gmail.com>
2025-01-23 13:54:25 -0500

    Update Config.php

--- config/Config.php
+++ config/Config.php
@@ -3,6 +3,9 @@
 define("CONTROLADOR_PRINCIPAL", "index");
 define("FUNCION_PRINCIPAL", "index");
 
+//ruta de vistas modulo de herramientas
+define("VHERRAMIENTAS", "view/herramientas/herramientas.");
+
 //ruta de vistas modulo de publicaciones
 define("VPUBLICACIONES", "view/publicaciones/publicaciones.");
 
