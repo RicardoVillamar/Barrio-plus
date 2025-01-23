5667558fd1bd9bccd48692c0627a68ec700c82dc
AndresFr56 <andresfreirech56@outlook.com>
2025-01-23 15:28:33 -0500

    Carpeta assets: css e images
    Carpeta assets: css e images

--- config/Config.php
+++ config/Config.php
@@ -3,6 +3,10 @@
 define("CONTROLADOR_PRINCIPAL", "index");
 define("FUNCION_PRINCIPAL", "index");
 
+//ruta de templates
+define("HEADER", 'view/templates/header.php');
+define("FOOTER", 'view/templates/footer.php');
+
 //ruta de vistas modulo de herramientas
 define("VHERRAMIENTAS", "view/herramientas/herramientas.");
 
