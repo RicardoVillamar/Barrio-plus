fd3d8b0a60d96170941dce636d64b8ef5d534060
AndresFr56 <andresfreirech56@outlook.com>
2025-01-23 15:32:51 -0500

    Update index controller
    Update index controller

--- controller/IndexController.php
+++ controller/IndexController.php
@@ -1,15 +1,22 @@
 <?php
+function limpiar($dato)
+{
+    $dato = trim($dato);
+    $dato = stripcslashes($dato);
+    $dato = htmlspecialchars($dato);
+    return $dato;
+}
+
 class IndexController {  
     public function index(){
         if(!empty($_GET['p'])){
-            $page =  $_GET['p']; // limpiar datos
+            $page =  limpiar($_GET['p']); // limpiar datos
             // flujo de ventanas
             require_once 'view/estaticas/'.$page.'.php';
            
         }else{
               // flujo de ventanas
-          require_once 'view/homeView.php'; //mostrando la vista de home de la aplicacion
-         
+          require_once 'view/homeView.php'; 
         }
     }   
 }
