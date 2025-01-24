bbd0dc349c8f2752ef4a633946413dbcf449a3c0
Roitdres <roitman.palaciosher@ug.edu.ec>
2025-01-23 22:33:52 -0500

    Update registro and reseva

--- view/usuario/registro.php
+++ view/usuario/registro.php
@@ -42,20 +42,36 @@
         }
     </style>
 
-
-    <div class="registro-container">
-        <h2>Registro de Usuario</h2>
-        <form>
-            <input type="text" placeholder="Nombre" >
-            <input type="text" placeholder="Apellido" >
-            <input type="email" placeholder="Correo Electrónico" >
-            <input type="password" placeholder="Contraseña" >
-            <select required>
-                <option value="">Seleccionar Rol</option>
-                <option value="1">Administrador</option>
-                <option value="2">Usuario</option>
-                <option value="3">Invitado</option>
-            </select>
-            <button  type="submit" >Registrarse</button>
-        </form>
+<section>
+<h1 class="titulos">Registro</h1>
+<form action="index.php?c=usuarios&f=registrar" method="POST">
+    <h2 class="subtitulos">Registro de Usuario</h2>
+    
+    <div>
+        <label for="nombre">Nombres:</label><br>
+        <input type="text" id="nombre" name="nombre" placeholder="Ingrese su nombre" required>
     </div>
+
+    <div>
+        <label for="apellido">Apellidos:</label><br>
+        <input type="text" id="apellido" name="apellido" placeholder="Ingrese sus apellidos" required>
+    </div>
+
+    <div>
+        <label for="correo">Correo:</label><br>
+        <input type="email" id="correo" name="correo" placeholder="Ingrese su correo" required>
+    </div>
+
+    <div>
+        <label for="contrasena">Contraseña:</label><br>
+        <input type="password" id="contrasena" name="contrasena" placeholder="Ingrese su contraseña" required>
+    </div>
+
+ 
+
+    <div>
+        <button type="submit">Registrarse</button>
+        <button type="reset">Cancelar</button>
+    </div>
+</form>
+</section>
