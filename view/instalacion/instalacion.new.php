2338ddee4cfd75a1039d0144560059d2d0fa8ee9
Ricardo <ricardo.villamarmin@ug.edu.ec>
2025-01-24 13:00:57 -0500

    Update instalacion.new.php

--- view/instalacion/instalacion.new.php
+++ view/instalacion/instalacion.new.php
@@ -4,13 +4,69 @@ require_once HEADER
 ?>
 
 <main>
+    <section class="contenedor-formulario">
 
-    <form action="">
+        <form action="" id="formulario-instalaciones" style="display: flex; flex-direction: column; gap: 10px;">
+            <div style="display: flex; align-items: center;">
+                <a href="index.php?c=instalacion&f=index_instalacion" style="padding: 0px; margin-left: 10px">
+                    <span class="material-symbols-outlined"> arrow_back_ios_new </span>
+                </a>
+                <h3 style="margin-left: 10px;">Registro de instalacion</h3>
+            </div>
 
-        <h3>Registro de instalacion</h3>
+            <div>
+                <label for="nombre">Nombre</label>
+                <input type="text" id="nombre" name="nombre" class="input" placeholder="Nombre de la instalacion">
+            </div>
 
-    </form>
+            <div>
+                <label for="descripcion">Descripcion</label>
+                <textarea id="descripcion" name="descripcion" placeholder="Descripcion de la instalacion"></textarea>
+            </div>
 
+            <div>
+                <label for="precio">Precio</label>
+                <input type="number" id="precio" name="precio" class="input" placeholder="Precio de la instalacion">
+            </div>
+
+            <div>
+                <label for="tamano">Tamaño</label>
+                <input type="text" id="tamano" name="tamano" class="input" placeholder="Tamaño de la instalacion">
+            </div>
+
+            <div>
+                <label for="tipo">Tipo</label>
+                <select id="tipo" name="tipo" class="input-selected">
+                    <option value="1">Cancha</option>
+                    <option value="2">Piscina</option>
+                    <option value="3">Gimnasio</option>
+                    <option value="4">Salon de eventos</option>
+                </select>
+            </div>
+
+            <div>
+                <label for="estado">Estado</label>
+                <select id="estado" name="estado" class="input-selected">
+                    <option value="1">Disponible</option>
+                    <option value="2">No disponible</option>
+                </select>
+            </div>
+
+            <div>
+                <label for="contribuidor">Contribuidor</label>
+                <input type="text" id="nombre" name="nombre" class="input" placeholder="Nombre de la instalacion">
+            </div>
+
+            <div>
+                <label for="imagen">Imagen</label>
+                <input type="file" id="imagen" name="imagen" class="input">
+            </div>
+
+            <div>
+                <button type="submit" class="boton-mediano">Registrar</button>
+            </div>
+        </form>
+    </section>
 </main>
 
 
