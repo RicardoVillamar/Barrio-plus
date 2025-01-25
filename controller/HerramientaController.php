5a04ab4c69906d9dae4166ca36e1fe5b79ec81a5
Yovel-Kyr <ajoel232003@gmail.com>
2025-01-24 23:37:45 -0500

    Update HerramientaController.php

--- controller/HerramientaController.php
+++ controller/HerramientaController.php
@@ -2,19 +2,24 @@
 //autor: Quiñonez Castrellón Anthony Joel
 require_once 'model/dto/Herramienta.php';
 require_once 'model/dao/HerramientaDAO.php';
+require_once 'model/dao/EstadoDAO.php';
+require_once 'model/dao/ContribucionDAO.php';
+
 
 class HerramientaController
 {
 
-    private $model;
+    private $model,$modeloEstado,$modeloContri;
 
     public function __construct()
     {
         $this->model = new HerramientaDAO();
+        $this->modeloEstado = new EstadoDAO();
+        $this->modeloContri = new ContribucionDAO();
+
     }
 
-    public function index()
-    {
+    public function index(){
         $resultado = $this->model->selectAll();
 
         $titulo = 'Herramientas';
@@ -22,13 +27,84 @@ class HerramientaController
         require_once VHERRAMIENTAS . 'list.php';
     }
 
+    public function index_Reserva(){
+    $resultadoCompleto = $this->model->selectAll();
+    $resultado = [];
+    foreach ($resultadoCompleto as $row) {
+        $resultado[] = [
+            'idEstadoFK' => $row['idEstadoFK'],
+            'nombre' => $row['nombre'],
+            'imagen' => $row['imagen'],
+            'descripcion' => $row['descripcion'],
+            'precio' => $row['precio'],
+            'idHerramienta' => $row['idHerramienta']
+
+        ];
+    }
+
+    $estados = $this->modeloEstado->selectEstado();
+    $titulo = 'Herramientas';
+
+    require_once VHERRAMIENTASRESERVA . 'list.php';
+}
+
+    public function view_reservar()
+    {
+        $id = htmlentities($_GET['id']);
+        
+        $herramienta=$this->model->selectOne($id);
+
+        $titulo = 'Reservar de Herramienta';
+
+        require_once VHERRAMIENTASRESERVA  . 'new.php';
+    }
+
+    
+    public function view_new(){
+        $herramienta= $this->model->selectAll();
+        $titulo = "Nueva herramienta";
+        require_once VHERRAMIENTAS . 'new.php';
+    }
+
+    public function view_edit(){
+        $id = htmlentities($_GET["id"]);
+        $herramienta = $this->model->selectOne($id);
+        if ($herramienta == null) {
+            $this->redirectWithMessage(false, "", "No se pudo encontrar la herramienta a editar", 
+            "index.php?c=herramienta&f=index");
+        }
+        $titulo = "Editar herramienta";
+        require_once VHERRAMIENTAS . 'edit.php';
+    }
+    
+    public function populate(){
+        $herramienta = new Herramienta();
+        $herramienta->setId(htmlentities($_POST['id'] ?? null));
+        $herramienta->setNombre(htmlentities($_POST['nombre']));
+        $herramienta->setDescrip(htmlentities($_POST['descripcion']));
+        $herramienta->setPrecio(htmlentities($_POST['precio']));
+        $herramienta->setImg($_FILES['imagen']['name'] ?? "");
+        $herramienta->setFechaRegis(htmlentities($_POST['fechaRegistro'] ?? date("Y-m-d")));
+        $herramienta->setIdEst(htmlentities($_POST['estado']));
+        $herramienta->setMant(htmlentities($_POST['mantenimiento']));
+        $herramienta->setCant(htmlentities($_POST['cantidad']));
+        $herramienta->setIdContri(htmlentities($_POST['contribuidor']));
+        return $herramienta;
+    }
+    
+    public function redirectWithMessage($exito, $exitoMsg, $errMsg, $redirectUrl){
+        if (!isset($_SESSION)) session_start();
+        $_SESSION['mensaje'] = ($exito) ? $exitoMsg : $errMsg;
+        $_SESSION['color'] = ($exito) ? 'primary' : 'danger';
+        header("Location: $redirectUrl");
+    }
     public function search(){
         $parametro = htmlentities($_POST['b'] ?? "");
         $resultado = $this->model->buscar($parametro);
         $titulo = "Buscar herramientas";
         require_once VHERRAMIENTAS . 'list.php';
     }
-    
+
     public function delete(){
         $id = htmlentities($_REQUEST['id'] ?? "");
 
@@ -46,73 +122,35 @@ class HerramientaController
             "index.php?c=herramienta&f=index"
         );
     }
-    
-    public function redirectWithMessage($exito, $exitoMsg, $errMsg, $redirectUrl){
-        if (!isset($_SESSION)) session_start();
-        $_SESSION['mensaje'] = ($exito) ? $exitoMsg : $errMsg;
-        $_SESSION['color'] = ($exito) ? 'primary' : 'danger';
-        header("Location: $redirectUrl");
+
+    public function insert()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $herramienta = [
+                'nombre' => $_POST['nombre'],
+                'descripcion' => $_POST['descripcion'],
+                'precio' => $_POST['precio'],
+                'imagen' => null,
+                'fechaRegistro' => $_POST['fechaRegistro'],
+                'idEstadoFK' => $_POST['estado'],
+                'mantenimiento' => $_POST['mantenimiento'],
+                'cantidad' => $_POST['cantidad'],
+                'idContribuidorFK' => $_POST['contribuidor'],
+            ];
+
+            if (isset($_FILES['imagen']) && $_FILES['imagen']['error'] === UPLOAD_ERR_OK) {
+                $file = $_FILES['imagen']['tmp_name'];
+                $herramienta['imagen'] = file_get_contents($file);
+            }
+
+            $resultado = $this->model->insert($herramienta);
+
+            if ($resultado) {
+                header('Location: index.php?c=herramienta&f=index');
+            } else {
+                echo "Error al registrar la instalación";
+            }
+        }
     }
-    
-    public function view_new(){
-        $titulo = "Nueva herramienta";
-        require_once VHERRAMIENTAS . 'new.php';
-    }
-    
-    public function new(){
-        if ($_SERVER["REQUEST_METHOD"] != "POST") {
-            $this->redirectWithMessage(false, "", "Método no permitido", 
-            "index.php?c=herramienta&f=index");
-        }
-        
-        if (empty($_POST["nombre"]) || empty($_POST["descripcion"])) {
-            $this->redirectWithMessage(false, "", "Datos incompletos", "index.php?c=herramienta&f=index");
-        }
-        $herramienta = $this->populate();
-        $exito = $this->model->insert($herramienta);
-        $this->redirectWithMessage($exito, "Herramienta insertada exitosamente", 
-        "No se pudo realizar la inserción", "index.php?c=herramienta&f=index");
-    }
-    
-    public function view_edit(){
-        $id = htmlentities($_GET["id"]);
-        $herramienta = $this->model->selectOne($id);
-        if ($herramienta == null) {
-            $this->redirectWithMessage(false, "", "No se pudo encontrar la herramienta a editar", 
-            "index.php?c=herramienta&f=index");
-        }
-        $titulo = "Editar herramienta";
-        require_once VHERRAMIENTAS . 'edit.php';
-    }
-    
-    public function edit(){
-        if ($_SERVER["REQUEST_METHOD"] != "POST") {
-            $this->redirectWithMessage(false, "", "Método no permitido", "index.php?c=herramienta&f=index");
-        }
-        
-        if (empty($_POST["nombre"]) || empty($_POST["descripcion"])) {
-            $this->redirectWithMessage(false, "", "Datos incompletos", "index.php?c=herramienta&f=index");
-        }
-        
-        $herramienta = $this->populate();
-        $exito = $this->model->update($herramienta);
-        $this->redirectWithMessage($exito, "Herramienta actualizada exitosamente", 
-        "No se pudo realizar la actualización", "index.php?c=herramienta&f=index");
-    }
-    
-    public function populate(){
-        $herramienta = new Herramienta();
-        $herramienta->setId(htmlentities($_POST['id'] ?? null));
-        $herramienta->setNombre(htmlentities($_POST['nombre']));
-        $herramienta->setImg($_FILES['imagen']['name'] ?? ""); // Verifica el archivo cargado
-        $herramienta->setDescrip(htmlentities($_POST['descripcion']));
-        $herramienta->setPrecio(htmlentities($_POST['precio']));
-        $herramienta->setFechaRegis(htmlentities($_POST['fechaRegistro'] ?? date("Y-m-d")));
-        $herramienta->setIdEst(htmlentities($_POST['estado']));
-        $herramienta->setMant(isset($_POST['mantenimiento']) ? 1 : 0);
-        $herramienta->setCant(htmlentities($_POST['cantidad']));
-        $herramienta->setIdContri(htmlentities($_POST['idContribuidorFK']));
-        return $herramienta;
-        }
 }
 ?>
