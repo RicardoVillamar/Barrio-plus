ec4827c376d3069e432ecc7e0569348494bf2acc
Ricardo <ricardo.villamarmin@ug.edu.ec>
2025-01-25 03:14:37 -0500

    Update barrio.sql

--- db/barrio.sql
+++ db/barrio.sql
@@ -34,7 +34,7 @@ CREATE TABLE `Herramienta` (
   `imagen` LONGBLOB,
   `fechaRegistro` DATE NOT NULL,
   `idEstadoFK` INT NOT NULL,
-  `mantenimiento` BOOLEAN DEFAULT false,
+  `mantenimiento` VARCHAR(150) NOT NULL,
   `cantidad` INT NOT NULL
 );
 
@@ -47,15 +47,6 @@ CREATE TABLE `Instalacion` (
   `idTipoFK` INT NOT NULL,
   `idEstadoFK` INT NOT NULL,
   `imagen` LONGBLOB
-);
-
-CREATE TABLE `Contribucion` (
-  `idContribucion` INT PRIMARY KEY AUTO_INCREMENT,
-  `estado` VARCHAR(50) NOT NULL,
-  `tipo` VARCHAR(50) NOT NULL,
-  `idHerramientaFK` INT,
-  `idInstalacionFK` INT,
-  `idUsuarioFK` INT NOT NULL
 );
 
 CREATE TABLE `ReservacionHerramienta` (
@@ -82,47 +73,60 @@ CREATE TABLE `ReservacionInstalacion` (
   `proposito` TEXT
 );
 
-CREATE TABLE `Publicacion` (
-  `idPublicacion` INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE TipoPublicacion (
+  `idTipo` INT AUTO_INCREMENT PRIMARY KEY,
+  `nombreTipo` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Prioridad (
+  `idPrioridad` INT AUTO_INCREMENT PRIMARY KEY,
+  `nivel` VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Publicacion ( 
+  `idPubli` INT AUTO_INCREMENT PRIMARY KEY,
+  `titulo` VARCHAR(30) NOT NULL,
+  `idTipoFK` INT NOT NULL,
+  `descripcion` VARCHAR(100) NOT NULL,
+  `idPrioridadFK` INT NOT NULL,
+  `fechaEvento` DATE NOT NULL,
+  `notificarAdmin` INT(1) NOT NULL,
+  `idUsuarioFK` INT NOT NULL
+);
+
+CREATE TABLE `Contribucion` (
+  `idContribucion` INT PRIMARY KEY AUTO_INCREMENT,
+  `estado` VARCHAR(50) NOT NULL,
+  `idHerramientaFK` INT,
+  `idInstalacionFK` INT,
   `idUsuarioFK` INT NOT NULL,
-  `contenido` TEXT NOT NULL,
-  `fecha` DATE NOT NULL
+  FOREIGN KEY (`idHerramientaFK`) REFERENCES `Herramienta` (`idHerramienta`),
+  FOREIGN KEY (`idInstalacionFK`) REFERENCES `Instalacion` (`idInstalacion`)
 );
 
 ALTER TABLE `Usuario` ADD FOREIGN KEY (`idRolFK`) REFERENCES `RolUsuario` (`idRol`);
-
 ALTER TABLE `Herramienta` ADD FOREIGN KEY (`idEstadoFK`) REFERENCES `Estado` (`idEstado`);
-
 ALTER TABLE `Instalacion` ADD FOREIGN KEY (`idTipoFK`) REFERENCES `Tipo` (`idTipo`);
-
 ALTER TABLE `Instalacion` ADD FOREIGN KEY (`idEstadoFK`) REFERENCES `Estado` (`idEstado`);
-
-ALTER TABLE `Contribucion` ADD FOREIGN KEY (`idHerramientaFK`) REFERENCES `Herramienta` (`idHerramienta`);
-
-ALTER TABLE `Contribucion` ADD FOREIGN KEY (`idInstalacionFK`) REFERENCES `Instalacion` (`idInstalacion`);
-
-ALTER TABLE `Contribucion` ADD FOREIGN KEY (`idUsuarioFK`) REFERENCES `Usuario` (`idUsuario`);
-
 ALTER TABLE `ReservacionHerramienta` ADD FOREIGN KEY (`idHerramientaFK`) REFERENCES `Herramienta` (`idHerramienta`);
-
 ALTER TABLE `ReservacionHerramienta` ADD FOREIGN KEY (`idUsuarioFK`) REFERENCES `Usuario` (`idUsuario`);
-
 ALTER TABLE `ReservacionInstalacion` ADD FOREIGN KEY (`idInstalacionFK`) REFERENCES `Instalacion` (`idInstalacion`);
-
 ALTER TABLE `ReservacionInstalacion` ADD FOREIGN KEY (`idUsuarioFK`) REFERENCES `Usuario` (`idUsuario`);
-
 ALTER TABLE `Publicacion` ADD FOREIGN KEY (`idUsuarioFK`) REFERENCES `Usuario` (`idUsuario`);
+ALTER TABLE `Publicacion` ADD FOREIGN KEY (`idTipoFK`) REFERENCES `TipoPublicacion` (`idTipo`);
+ALTER TABLE `Publicacion` ADD FOREIGN KEY (`idPrioridadFK`) REFERENCES `Prioridad` (`idPrioridad`);
 
 
---INSERT INTO `Estado` (`nombre`) VALUES ('Libre'), ('Ocupado');
---INSERT INTO `Tipo` (`nombre`) VALUES ('Aire libre'), ('Aula'), ('Salon'), ('Taller');
---INSERT INTO `rolusuario` (`nombre`) VALUES ('Admin'), ('Vecino'), ('Contribuidor');
+INSERT INTO `Estado` (`nombre`) VALUES ('Libre'), ('Ocupado');
+INSERT INTO `Tipo` (`nombre`) VALUES ('Aire libre'), ('Aula'), ('Salon'), ('Taller');
+INSERT INTO `rolusuario` (`nombre`) VALUES ('Admin'), ('Vecino'), ('Contribuidor');
 
---INSERT INTO `Prioridad` (`nivel`) VALUES ('Alta'), ('Media'), ('Baja');
---INSERT INTO `TipoPublicacion` (`nombreTipo`) VALUES ('Reporte de daños'), ('Avisos generales'), ('Avisos de mantenimiento'), ('Solicitudes de recursos');
 
-/*
---Inserts de prueba tabla de usuario
+INSERT INTO `Prioridad` (`nivel`) VALUES ('Alta'), ('Media'), ('Baja');
+INSERT INTO `TipoPublicacion` (`nombreTipo`) VALUES ('Reporte de daños'), ('Avisos generales'), ('Avisos de mantenimiento'), ('Solicitudes de recursos');
+
+
+
 INSERT INTO `Usuario` (`nombre`, `apellido`, `correo`, `contrasena`, `idRolFK`) VALUES
 ('Miguel', 'Sanchez', 'miguel.sanchez@hotmail.com', 'migueADM', 1),
 ('Luisa', 'Garcia', 'luisa.garcia@outlook.com', 'garciaLuisa', 2), 
@@ -131,11 +135,10 @@ INSERT INTO `Usuario` (`nombre`, `apellido`, `correo`, `contrasena`, `idRolFK`) 
 ('Juan', 'Martinez', 'juan.martinez@gmail.com', 'juADM', 3); 
 
 
--- Inserciones de prueba tabla Publicacion
+
 INSERT INTO `Publicacion` (`titulo`, `idTipoFK`, `descripcion`, `idPrioridadFK`, `fechaEvento`, `notificarAdmin`, `idUsuarioFK`) VALUES
 ('Reparación de alumbrado', 1, 'Se requiere reparar un poste de luz en la calle principal.', 1, '2025-02-01', 1, 4), 
 ('Taller de reciclaje', 4, 'Organización de un taller de reciclaje en el salón comunitario.', 2, '2025-02-10', 0, 3),
 ('Aviso de mantenimiento', 3, 'Cierre temporal del parque por mantenimiento.', 3, '2025-01-30', 1, 2), 
 ('Fugas de agua', 1, 'Reporte de fuga de agua cerca del aula 2.', 1, '2025-01-29', 1, 4), 
 ('Anuncio de reunión', 2, 'Aviso para reunión general de vecinos el próximo viernes.', 2, '2025-02-05', 0, 5); 
-*/
