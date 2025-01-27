2ec06f3c99837aec4704db4649063223263c56d1
Ricardo <ricardo.villamarmin@ug.edu.ec>
2025-01-26 22:09:45 -0500

    Update instalacion.list.php

--- view/instalacion/instalacion.list.php
+++ view/instalacion/instalacion.list.php
@@ -3,9 +3,7 @@
 require_once HEADER
 ?>
 <div class="cabecera-reserva">
-    <a href="index.php?c=instalacion&f=index" style="padding: 0px; margin-left: 10px">
-        <span class="material-symbols-outlined"> arrow_back_ios_new </span>
-    </a>
+
     <h1 style="padding: 0px 10px; font-size: 1.5rem;">
         Instalaciones
     </h1>
